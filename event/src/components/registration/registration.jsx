0deaa4e8523c6190f250d014b32f7634cbad7a19
ASWIN V K <vkaswinkanan@gmail.com>
2024-07-24 16:16:21 +0530

    update

--- event/src/components/registration/registration.jsx
+++ event/src/components/registration/registration.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import QRCode from "qrcode.react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 import "./registration.css";
@@ -13,12 +13,28 @@ const RegistrationPage = () => {
   const [phone, setPhone] = useState("");
   const [regNo, setRegNo] = useState("");
   const [course, setCourse] = useState("");
+  const [program, setProgram] = useState("");
+  const [bloodGroup, setBloodGroup] = useState("");
   const [hORd, setHorD] = useState("");
-  const [hostelID, setHostelID] = useState("");
+  const [hostelNo, setHostelNo] = useState("");
   const [upiLink, setUpiLink] = useState("");
   const [paymentScreenshot, setPaymentScreenshot] = useState(null);
   const [paymentConfirmed, setPaymentConfirmed] = useState(false);
   const [showPaymentOptions, setShowPaymentOptions] = useState(false);
+
+  useEffect(() => {
+    if (hORd === "Hosteller") {
+      if (gender === "male") {
+        setHostelNo("DH");
+      } else if (gender === "female") {
+        setHostelNo("GH");
+      } else {
+        setHostelNo("");
+      }
+    } else {
+      setHostelNo("");
+    }
+  }, [gender, hORd]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -29,9 +45,11 @@ const RegistrationPage = () => {
       !email ||
       !phone ||
       !regNo ||
+      !bloodGroup||
+      !program ||
       !course ||
       !hORd ||
-      (hORd === "Hosteller" && !hostelID)
+      (hORd === "Hosteller" && !hostelNo)
     ) {
       alert("Please fill all the fields");
       return;
@@ -45,8 +63,10 @@ const RegistrationPage = () => {
     formData.append("phone", phone);
     formData.append("regNo", regNo);
     formData.append("course", course);
+    formData.append("program", program);
+    formData.append("bloodGroup", bloodGroup);
     formData.append("hORd", hORd);
-    formData.append("hostelID", hostelID);
+    formData.append("hostelNo", hostelNo);
     if (paymentScreenshot) {
       formData.append("paymentScreenshot", paymentScreenshot);
     }
@@ -54,7 +74,7 @@ const RegistrationPage = () => {
     axios
       .post("http://localhost:5000/register", formData)
       .then(() => {
-        alert("Registration successful! Please complete the payment.");
+        alert("Registration successful!");
       })
       .catch((error) => {
         console.error("Error storing data: ", error);
@@ -128,7 +148,7 @@ const RegistrationPage = () => {
             country={"in"}
             value={phone}
             onChange={(phone) => setPhone(phone)}
-            inputStyle={{width:"90%"}}
+            inputStyle={{ width: "90%" }}
           />
         </div>
         <div className="field">
@@ -150,7 +170,7 @@ const RegistrationPage = () => {
           />
         </div>
         <div className="field">
-          <h3 className="field-title">Course</h3>
+          <h3 className="field-title">Program</h3>
           <select
             className="field-input"
             value={course}
@@ -159,8 +179,32 @@ const RegistrationPage = () => {
             <option value="">Select</option>
             <option value="B.Tech">B.Tech</option>
             <option value="M.Tech">M.Tech</option>
+            <option value="BCA">BCA</option>
+            <option value="MCA">MCA</option>
+            <option value="B.Com">B.com</option>
+            <option value="B.Sc">B.sc</option>
+            <option value="M.Sc">M.sc</option>
+            <option value="BBA">BBA</option>
             <option value="MBA">MBA</option>
           </select>
+        </div>
+        <div className="field">
+          <h3 className="field-title">Specify program</h3>
+          <input
+            type="text"
+            className="field-input"
+            value={program}
+            onChange={(e) => setProgram(e.target.value)}
+          />
+        </div>
+        <div className="field">
+          <h3 className="field-title">Blood Group</h3>
+          <input
+            type="text"
+            className="field-input"
+            value={bloodGroup}
+            onChange={(e) => setBloodGroup(e.target.value)}
+          />
         </div>
         <div className="field">
           <h3 className="field-title">Hosteller/Dayscholar</h3>
@@ -176,11 +220,11 @@ const RegistrationPage = () => {
         </div>
         {hORd === "Hosteller" && (
           <div className="field">
-            <h3 className="field-title">Hostel ID</h3>
+            <h3 className="field-title">Hostel Number</h3>
             <input
               type="text"
-              value={hostelID}
-              onChange={(e) => setHostelID(e.target.value)}
+              value={hostelNo}
+              onChange={(e) => setHostelNo(e.target.value)}
               className="field-input"
             />
           </div>
